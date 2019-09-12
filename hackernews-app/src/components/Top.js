6f5d54e29db36e81e8962a6bcc2b83908bf7412c
olympia <owojcik7@gmail.com>
2019-09-12 13:27:44 -0400

    Format time using moment.js and display # of comments

--- hackernews-app/src/components/Top.js
+++ hackernews-app/src/components/Top.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { fetchTopItems, fetchStory } from "../utils/api";
+var moment = require("moment");
 
 class Top extends React.Component {
   constructor(props) {
@@ -24,9 +25,10 @@ class Top extends React.Component {
       <div className="App">
         <h1>Axios vs. Fetch</h1>
         {this.state.topStories.map(story => {
+          //   console.log(story);
           const {
             by,
-            descendents,
+            descendants,
             id,
             kids,
             score,
@@ -37,16 +39,18 @@ class Top extends React.Component {
           } = story;
 
           var date = new Date(time * 1000).toLocaleDateString("en-US");
-          let dateTime = new Date(time * 1000).toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit"
-          });
+          //   let dateTime = new Date(time * 1000).toLocaleTimeString([], {
+          //     hour: "2-digit",
+          //     minute: "2-digit"
+          //   });
 
+          let dateTime = new Date(time * 1000);
+          dateTime = moment(dateTime).format("H:mm A");
           return (
             <>
               <h2>{title}</h2>
               <p>
-                by {by} on {date} {dateTime}
+                by {by} on {date}, {dateTime} with {descendants} comments
               </p>
             </>
           );
