bca5609b44244545b4f97c30d510ef9b5188fbf2
olympia <owojcik7@gmail.com>
2019-09-11 21:49:31 -0400

    Map over array of stories and render each story

--- hackernews-app/src/components/Top.js
+++ hackernews-app/src/components/Top.js
@@ -2,10 +2,19 @@ import React from "react";
 import { fetchTopItems, fetchStory } from "../utils/api";
 
 class Top extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topStories: [],
+      loading: false
+    };
+  }
   componentDidMount() {
     fetchTopItems().then(ids =>
       ids.map(id => {
-        fetchStory(id).then(res => console.log(res));
+        fetchStory(id).then(story => {
+          this.setState({ topStories: [...this.state.topStories, story] });
+        });
       })
     );
   }
@@ -14,6 +23,34 @@ class Top extends React.Component {
     return (
       <div className="App">
         <h1>Axios vs. Fetch</h1>
+        {this.state.topStories.map(story => {
+          const {
+            by,
+            descendents,
+            id,
+            kids,
+            score,
+            time,
+            title,
+            type,
+            url
+          } = story;
+
+          var date = new Date(time * 1000).toLocaleDateString("en-US");
+          let dateTime = new Date(time * 1000).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit"
+          });
+
+          return (
+            <>
+              <h2>{title}</h2>
+              <p>
+                by {by} on {date} {dateTime}
+              </p>
+            </>
+          );
+        })}
       </div>
     );
   }
