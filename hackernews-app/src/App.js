1591e0d8ca4d2abcc8c3e314f447b06e54ce2563
olympia <owojcik7@gmail.com>
2019-09-15 15:41:00 -0400

    Use render props to share Posts.js component and use to render Top and New stories

--- hackernews-app/src/App.js
+++ hackernews-app/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import Top from "./components/Top";
-import New from "./components/New";
+import Posts from "./components/Posts.js";
 import Nav from "./components/Nav";
 
 class App extends React.Component {
@@ -12,8 +11,16 @@ class App extends React.Component {
       <Router>
         <div className="App">
           <Nav></Nav>
-          <Route exact path="/" render={() => <Top type="top"></Top>}></Route>
-          <Route exact path="/new" component={New}></Route>
+          <Route
+            exact
+            path="/"
+            render={() => <Posts type="top"></Posts>}
+          ></Route>
+          <Route
+            exact
+            path="/new"
+            render={() => <Posts type="new"></Posts>}
+          ></Route>
         </div>
       </Router>
     );
