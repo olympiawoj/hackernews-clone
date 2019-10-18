184263592233f762706785cb3aa2b1886ebfcf76
olympia <owojcik7@gmail.com>
2019-10-18 09:26:22 -0400

    Add NavLink styling to Nav

--- hackernews-app/src/components/Nav.js
+++ hackernews-app/src/components/Nav.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
     <div className="nav">
-      <Link className="nav-link" to="/">
+      <NavLink activeClassName="active" className="nav-link" exact to="/">
         Top
-      </Link>
-      <Link className="nav-link" to="/new">
+      </NavLink>
+      <NavLink activeClassName="active" className="nav-link" to="/new">
         New
-      </Link>
+      </NavLink>
     </div>
   );
 }
