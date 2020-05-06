449bd538c9e6c0930549821a6099857ca9bdfc88
olympia <owojcik7@gmail.com>
2020-05-06 14:02:09 -0400

    Fixed styling in dark mode of a post's comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,18 +1,26 @@
 import React from "react";
 import PostMetaInfo from "./PostsMetaInfo";
+import { ThemeConsumer } from "../contexts/theme"
 
 export default function Comment(props) {
   console.log(props.comment);
   const { comment } = props;
   return (
-    <div className="post">
-      <PostMetaInfo
-        comment={true}
-        by={comment.by}
-        time={comment.time}
-        id={comment.id}
-      ></PostMetaInfo>
-      <p dangerouslySetInnerHTML={{ __html: comment.text }}></p>
-    </div>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className={`post bg-medium-${theme}`}>
+          <PostMetaInfo
+            comment={true}
+            by={comment.by}
+            time={comment.time}
+            id={comment.id}
+            className={`bg-medium-${theme}`}
+          ></PostMetaInfo>
+          <p className={`text-light-${theme}`} dangerouslySetInnerHTML={{ __html: comment.text }} ></p>
+        </div>
+      )
+      }
+    </ThemeConsumer >
+
   );
 }
