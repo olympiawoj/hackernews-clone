531a209f638e8ae4d479e62df33ff08d2e6c522b
olympia <owojcik7@gmail.com>
2020-05-06 14:07:48 -0400

    Add conditional to show whether to display comments or not in PostMetaInfo

--- src/components/PostsMetaInfo.js
+++ src/components/PostsMetaInfo.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { formatDate, formatDatetime } from "../utils/helpers";
 import { ThemeConsumer } from "../contexts/theme"
 
-export default function PostMetaInfo({ by, time, id, decendants }) {
+export default function PostMetaInfo({ by, time, id, decendants, comment }) {
   let date = formatDate(time);
   let dateTime = formatDatetime(time);
 
@@ -11,10 +11,16 @@ export default function PostMetaInfo({ by, time, id, decendants }) {
     <ThemeConsumer>
       {({ theme }) => (
         <div>
-          <p className={`postMetaText  text-${theme}`} >
-            by <Link className={`postLink link-${theme}`} to={`/user?id=${by}`}>{by}</Link> on {date}, {dateTime} with {' '}
-            <Link className={`postLink link-${theme}`} to={`/post?id=${id}`}>{decendants}</Link> comments
-  </p>
+          {!comment ? (
+            <p className={`postMetaText  text-${theme}`} >
+              by <Link className={`postLink link-${theme}`} to={`/user?id=${by}`}>{by}</Link> on {date}, {dateTime} with {' '}
+              <Link className={`postLink link-${theme}`} to={`/post?id=${id}`}>{decendants}</Link> comments
+            </p>
+          ) : (
+              <p className={`postMetaText  text-${theme}`} >
+                by <Link className={`postLink link-${theme}`} to={`/user?id=${by}`}>{by}</Link> on {date}, {dateTime}
+              </p>
+            )}
         </div>
       )}
     </ThemeConsumer>
